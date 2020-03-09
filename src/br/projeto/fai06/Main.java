0be2404d755fb74e1d71b278c7db83cc5777049a
Ana Terezinha <aterezinha.19@hotmail.com>
2020-03-09 19:45:06 -0300

    Segundo commit]

---
+++ src/br/projeto/fai06/Main.java
@@ -0,0 +1,37 @@
+package br.projeto.fai06;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+	}
+
+	private void start() {
+	String array[] = new String[4];
+        
+        array[0] = "civic";
+        array[1] = "gol";
+        array[2] = "palio";
+        array[3] = "uno";
+        
+        for (String iteracao : array) {        	
+        	
+        	System.out.println("Nome do carro: "+iteracao);
+        	
+		}
+        
+        System.out.println("\n----------------------\n");
+        
+        for (String iteracao : array) {
+        	array[1] = "L200";
+            System.out.println("Nome do carro: "+iteracao);
+		}
+        for (String iteracao: array) {
+			array[array.length-1]="gol";
+			System.out.println(iteracao);
+		}
+        
+	}
+
+}
